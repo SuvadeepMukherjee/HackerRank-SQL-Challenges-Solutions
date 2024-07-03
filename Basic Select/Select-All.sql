67e17906dfbb059536ebd41010366c738335bf26
Suvadeep <suvadeepm0709@gmail.com>
2024-07-03 21:44:16 +0530

    Changed Code Formatter

--- Basic Select/Select-All.sql
+++ Basic Select/Select-All.sql
@@ -1,1 +1,4 @@
-SELECT * FROM CITY;
+SELECT
+  *
+FROM
+  CITY;
--- Basic Select/Weather-Observation-Station-4.sql
+++ Basic Select/Weather-Observation-Station-4.sql
@@ -1,1 +1,12 @@
-SELECT (SELECT COUNT(city) from STATION)-(SELECT  COUNT(DISTINCT city) FROM STATION)as Difference ;
+SELECT
+  (
+    SELECT
+      COUNT(city)
+    from
+      STATION
+  ) - (
+    SELECT
+      COUNT(DISTINCT city)
+    FROM
+      STATION
+  ) as Difference;
